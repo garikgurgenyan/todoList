f7a79732f30cb49fa68e240210c2dc68ee2cdfca
G. Garik <g.gurgenyan@gmail.com>
2016-02-12 12:11:08 +0400

    dump database-test

--- todolist.sql
+++ todolist.sql
@@ -1,16 +1,45 @@
---create table
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.11-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `todolist`
+--
+
+DROP TABLE IF EXISTS `todolist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `todolist` (
-   `id` int(11) NOT NULL AUTO_INCREMENT,
-   `note` varchar(100) NOT NULL,
-   `deadline` date NOT NULL,
-   `status` bit(1) DEFAULT b'0',
-   PRIMARY KEY (`id`)
- ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
- 
---insert data
-INSERT INTO `todolist` 
-VALUES 
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `note` varchar(100) NOT NULL,
+  `deadline` date NOT NULL,
+  `status` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todolist`
+--
+
+LOCK TABLES `todolist` WRITE;
+/*!40000 ALTER TABLE `todolist` DISABLE KEYS */;
+INSERT INTO `todolist` VALUES 
 (1,'Перемещение поля. Замени поля isManAnya и isManRoma полем man в нужном классе.','2016-02-15','\0')
 ,(2,'Извлечение класса.','2016-02-16','\0')
 ,(3,'Добавь класс Address в пакет user.','2015-02-02','')
@@ -21,3 +50,16 @@ VALUES
 ,(8,'Сокрытие делегирования.','2016-01-01','')
 ,(9,'Добавь в класс User метод getBoss().','2016-01-01','')
 ,(10,'Перепиши реализацию метода getBoosName(User user) класса UserHelper.','2016-01-01','');
+/*!40000 ALTER TABLE `todolist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-12  3:45:18
